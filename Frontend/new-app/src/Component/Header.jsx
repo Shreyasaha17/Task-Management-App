33d44f5d39c438e46300d840396befcb4e9f9989
Shreyasaha17 <shreyasaha1704@gmail.com>
2024-10-30 09:08:21 +0530

    Update navbar

--- Frontend/new-app/src/Component/Header.jsx
+++ Frontend/new-app/src/Component/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
 
 <Navbar expand="lg" className="bg-body-tertiary">
 <Container>
-  <Navbar.Brand href="#home">Task Management App</Navbar.Brand>
+  <Navbar.Brand >Task Management App</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="me-auto">
